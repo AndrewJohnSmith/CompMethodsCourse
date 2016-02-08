debdd23d09cc9e48bce675d34aff0ee0cea6a1b9
AndrewJohnSmith <ajsmith@bio.umass.edu>
2016-02-08 16:36:54 -0500

    Completed script.

--- Week3/MultipleRegression,ANCOVA,ModelSelection-2016.R
+++ Week3/MultipleRegression,ANCOVA,ModelSelection-2016.R
@@ -4,6 +4,7 @@
 
 # Import the mult_regr_data.csv data file
 setwd("/Users/Home/Dropbox/Amherst/Courses/CompMethods/CompMethodsCourse/Week3")
+require(car)
 mult_regr_data<-read.csv("mult_regr_data.csv", row.names=1)
 
 # Explore the data
@@ -13,6 +14,8 @@ str(mult_regr_data)
 summary(mult_regr_data)
 
 # Create a correlation matrix, similar to the one produced in Week 1
+cor(mult_regr_data)
+pairs(mult_regr_data)
 
 # Create a multiple regression using two predictor variables
 mult_reg_model01<-lm(MaxLongevity_m~Precip_Mean_mm+AdultBodyMass_g, data=mult_regr_data)
@@ -37,8 +40,10 @@ plot(mult_reg_model02)
 
 # Check the variance inflation factor values
 # first install the car package
+require(car)
 
 vif(mult_reg_model02)
+#Close to one so variables are not colinear
 
 # ANCOVA ####################################################
 # Use the brain_bodymass dataset for this section
@@ -47,6 +52,7 @@ vif(mult_reg_model02)
 # Tests for differences in the intercept (mean value) of groups. This is often used to test for grade shifts among clades.
 
 # Import the brain_bodymass.csv dataset
+brain_bodymass<-read.csv("brain_bodymass.csv", row.names=1)
 
 # Test for differences in the Y value among clades in the relationship between body mass and brain size.
 # In other words, for a given body mass, do different clades have different brain sizes?
@@ -76,7 +82,7 @@ interaction.plot(log10(brain_bodymass$mass_female), brain_bodymass$clade, log10(
 summary(ancova_model01)
 
 # How would you interpret the results?
-
+  #Significant difference in slopes b/w mass and Platyrrhine to Caterrine
 # If the interaction term is not signficant, then re-run the analysis without the interaction term.
 # In this example, assume no interaction term is significant
 
@@ -85,7 +91,7 @@ ancova_model02<-lm(log10(ecv_female)~log10(mass_female)+clade, data = brain_body
 summary(ancova_model02)
 
 # How would you interpret the results?
-
+  #Sig dif in mass between catherines and platyrrhines
 # Check the model assumptions with plot (); though first modify your graphics window
 par(mfrow=c(2,2))
 
@@ -104,7 +110,7 @@ plot(ancova_model02)
 # stepwise model selection by AIC  
 
 # Install the MASS package
-
+require(MASS)
 fit<-lm(MaxLongevity_m~SocialGrpSize+Precip_Mean_mm+AdultBodyMass_g+SexualMaturityAge_d, data=mult_regr_data)
 
 step <- stepAIC(fit, direction="both")
@@ -112,6 +118,7 @@ step <- stepAIC(fit, direction="both")
 step$anova # display results
 
 # Lower AIC models indicate a better model
+  #Precipitation predictor is not a decent addition to the model
 
 #########################################################################
 
@@ -119,6 +126,7 @@ step$anova # display results
 # uses AICc to judge the best models and best predictor variables 
 
 # Install MuMIn package
+require(MuMIn)
 model01x<-lm(log10(MaxLongevity_m)~log10(SocialGrpSize)+log10(Precip_Mean_mm)+log10(AdultBodyMass_g)+log10(SexualMaturityAge_d), data=mult_regr_data)
 
 options(na.action=na.fail) # Only performs analysis if no missing values are in dataset
@@ -134,8 +142,8 @@ dd_models  # This will list the specific variables in each model
 # Models within 2 AICc values of the best model are considered equally good
 # The "delta" column lists the difference in AICc value between that model and the best model
 
-importance(dd)  # Sum of ?Akaike weights? over all models that include the explanatory variable
+importance(dd)  # Sum of Akaike weights over all models that include the explanatory variable
 # Variables with an Akaike weight closer to 1 are more important than those with values close to 0
 
 # Which model(s) is the best? Which variable(s) is most important? Email me your answers.
-
+  #Model 5 was the best, adult body mass was the 'most important' variable
